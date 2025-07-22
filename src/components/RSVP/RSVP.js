3cf25802249ba462ddcb0b04d496f2120de8f97b
petershindp <ssm.peter.shin@gmail.com>
2025-07-21 17:14:47 -0700

    changed rsvp date to aug 10

--- src/components/RSVP/RSVP.js
+++ src/components/RSVP/RSVP.js
@@ -5,7 +5,7 @@ const RSVP = () => {
 		<div className="rsvp-section">
 			<div className="rsvp">
 				<h1 className="rsvp-title">We hope you can make it!</h1>
-				<h1 className="rsvp-text">Please RSVP by August 8.</h1>
+				<h1 className="rsvp-text">Please RSVP by August 10.</h1>
 			</div>
 		</div>
 	);
